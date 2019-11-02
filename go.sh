ca24ff1945ce9de811ab61b4961ce0ae6b06a763
marcus <marcus@hhost.me>
2019-11-02 13:19:20 +0000

    split voices

--- go.sh
+++ go.sh
@@ -2,5 +2,6 @@
 
 while ./clipnotify/clipnotify;
 do
-  node index.js | speak
+  node index.js | cat
+  node index.js | espeak -v gmw/en -g 01ms -a 110 -p 58 -s 175
 done
--- index.js
+++ index.js
@@ -8,21 +8,48 @@ function execute(command, callback){
     exec(command, function(error, stdout, stderr){ callback(stdout); });
 };
 
-execute("xsel", function(test){
+execute("xsel -o | cat", function(test){
   if (test) {
-    try {
-      const window = (new JSDOM(``, { runScripts: "outside-only" })).window;
-      window.eval(test)
-      text = window.document.querySelector("body").textContent;
-    } catch (e) {
-      if (!text) {
-        try {
-          text = eval(test)
-        } catch (e){
-      
+    testToEval = test.replace( /alert\(/g ,"console.log(")
+    if (test.indexOf("</script>") === -1 || test.indexOf("</body>") === -1) {
+      try {
+        const window = (new JSDOM(``, { runScripts: "outside-only" })).window;
+        window.eval(testToEval)
+        text = window.document.querySelector("body").textContent;
+      } catch (e) {
+        console.error(e) 
+        if (!text) {
+          try {
+            text = eval(testToEval)
+          } catch (e){
+            console.error(e) 
+          }
+        }
+      }
+    } else {
+      try {
+        const window = (new JSDOM(testToEval, { runScripts: "dangerously" })).window;
+        var scripts = [... window.document.querySelectorAll("script")];
+	scripts.forEach(elem => {
+          elem.parentNode.removeChild(elem);
+        })
+        text = window.document.querySelector("body").textContent;
+        
+      } catch (e) {
+        if (!text) {
+          try {
+            text = eval(testToEval)
+          } catch (e){
+            console.error(e) 
+          }
         }
       }
     }
-    console.log(text);
+    if (text) {
+      execute("espeak -ven-us+f4 -s170 \"" + text + "\"", () => {})
+     // console.log(text);
+    } else {
+      console.log(test);
+    }
   }
 })
